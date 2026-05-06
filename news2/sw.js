c8756407c070252ba19939a3cac7ba7ce4c1bdc3
ymatsuda <ymatsuda@rtarm.co.jp>
2026-05-06 10:11:47 +0900

    ニュース機能

---
+++ news2/sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'ai-news-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './app.js',
+    './manifest.json',
+    './icon-192.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
